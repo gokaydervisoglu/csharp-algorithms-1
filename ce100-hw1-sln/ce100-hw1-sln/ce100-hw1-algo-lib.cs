7eedf0104f6950b12977f3ddbf94a5050066f01e
mehmetaligumusler <gumuslermehmetali@gmail.com>
2023-03-12 22:03:56 +0300

    MergeSort Function Added

--- ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
+++ ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
@@ -45,4 +45,89 @@ namespace Sort
 
     }
 
+    public class MergeSort_Class
+    {
+        public static void merge(int[] arr, int p, int q, int r)
+        {
+
+
+            int n1 = q - p + 1;
+            int n2 = r - q;
+
+            int[] L = new int[n1];
+            int[] M = new int[n2];
+
+            for (int z = 0; z < n1; z++)
+
+                L[z] = arr[p + z];
+
+            for (int y = 0; y < n2; y++)
+                M[y] = arr[q + 1 + y];
+
+
+            int i, j, k;
+            i = 0;
+            j = 0;
+            k = p;
+
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= M[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = M[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = M[j];
+                j++;
+                k++;
+            }
+
+
+        }
+
+      
+
+        public static int[] mergeSort(int[] row, int l, int r)
+        {
+            int size = row.Length;
+
+            int[] result = new int[size];
+
+            if (l < r)
+            {
+
+                int m = l + (r - l) / 2;
+
+                mergeSort(row, l, m);
+                mergeSort(row, m + 1, r);
+
+                merge(row, l, m, r);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = row[i];
+            }
+
+            return result;
+        }
+
+    }
 }
