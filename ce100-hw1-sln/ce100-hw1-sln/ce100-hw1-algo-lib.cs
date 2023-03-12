2be19f693fc2ed446963e9703fe8fefb7e463b86
mehmetaligumusler <gumuslermehmetali@gmail.com>
2023-03-13 00:06:20 +0300

    Recursive Binary Search Function Added

--- ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
+++ ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
@@ -281,4 +281,28 @@ namespace BinarySearch
 
 
     }
+
+    public static class BinarySearchRecursive_Class
+    {
+     
+        public static int binarySearch(int[] arr, int left, int right, int x)
+        {
+            if (right >= left)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == x)
+                    return 0;
+
+                if (arr[mid] > x)
+                    return binarySearch(arr, left, mid - 1, x);
+
+
+                return binarySearch(arr, mid + 1, right, x);
+            }
+
+            return -1;
+        }
+
+    }
 }
