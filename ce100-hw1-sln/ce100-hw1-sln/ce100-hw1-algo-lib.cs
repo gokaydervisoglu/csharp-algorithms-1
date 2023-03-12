5e0222a6d532a7b3bd5f8041d4edff241a022078
gokaydervisoglu <gokaydervisoglu@gmail.com>
2023-03-12 22:00:05 +0300

    SelectionSort Function Added.

--- ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
+++ ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ce100_hw1_sln
+namespace Sort
 {
-    public class Class1
+    public class SelectionSort_Class
     {
+
+        public static void replace(ref int first, ref int second)
+        {
+            int r = first;
+
+            first = second;
+            second = r;
+        }
+
+        public static int[] Selectionsort(int[] row, int size)
+        {
+            int[] result = new int[size];
+
+            for (int j = 0; j < size - 1; j++)
+            {
+                int min = j;
+
+                for (int i = j + 1; i < size; i++)
+                {
+
+                    if (row[i] < row[min])
+                        min = i;
+                }
+
+                replace(ref row[min], ref row[j]);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = row[i];
+            }
+
+            return result;
+        }
+
     }
+
 }
