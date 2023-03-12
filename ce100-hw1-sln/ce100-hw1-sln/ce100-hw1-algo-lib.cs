ca0547f7a089fbd0d0e2f570f2c7566ccaaeef42
mehmetaligumusler <gumuslermehmetali@gmail.com>
2023-03-12 22:51:54 +0300

    QuickSort Lomuto Function Added

--- ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
+++ ce100-hw1-sln/ce100-hw1-sln/ce100-hw1-algo-lib.cs
@@ -190,4 +190,62 @@ namespace Sort
 
 
     }
+
+    public static class QuickSortLomuto_Class
+    {
+        
+        public static void swap(int[] array, int i, int j)
+        {
+
+            int temp = array[i];
+
+            array[i] = array[j];
+
+            array[j] = temp;
+        }
+      
+        public static int partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+
+            int i = (low - 1);
+
+            for (int j = low; j <= high - 1; j++)
+            {
+
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, high);
+            return (i + 1);
+        }
+     
+        public static int[] quickSort(int[] arr, int low, int high)
+
+        {
+            int size = arr.Length;
+
+            int[] result = new int[size];
+
+            if (low < high)
+            {
+
+                int pivot = partition(arr, low, high);
+
+                quickSort(arr, low, pivot - 1);
+                quickSort(arr, pivot + 1, high);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = arr[i];
+            }
+
+            return result;
+        }
+
+    }
 }
