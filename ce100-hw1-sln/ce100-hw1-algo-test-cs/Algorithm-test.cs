1a5d33f04757cb66241425a8f0edb9ad73010035
gokaydervisoglu <gokaydervisoglu@gmail.com>
2023-03-12 21:55:44 +0300

    First commit

---
+++ ce100-hw1-sln/ce100-hw1-algo-test-cs/Algorithm-test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ce100_hw1_algo_test_cs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
