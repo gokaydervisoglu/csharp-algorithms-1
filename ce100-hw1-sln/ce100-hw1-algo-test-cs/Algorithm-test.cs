60156ed4f8dbe26abbd5f3c1f579be28e18584f2
gokaydervisoglu <gokaydervisoglu@gmail.com>
2023-03-13 20:34:11 +0300

    SelectionSort Unit Test Added and Some Problems Fixed.

--- ce100-hw1-sln/ce100-hw1-algo-test-cs/Algorithm-test.cs
+++ ce100-hw1-sln/ce100-hw1-algo-test-cs/Algorithm-test.cs
@@ -1,13 +1,123 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sort;
+using BinarySearch;
+using MatrixMultiplication;
 
-namespace ce100_hw1_algo_test_cs
+
+namespace SelectionSort_TestClass
 {
+    // SelectionSort
+
+    /// <summary>
+    /// The purpose of the test here is to sort a nearly complete array and see the best method.
+    /// </summary>
+
     [TestClass]
-    public class UnitTest1
+    public class BestSelectionSortTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSelectionSortMethod()
         {
+
+            int size;
+
+            int[] data2 = new int[10000];
+            Random random = new Random();
+
+            for (int i = 0; i < data2.Length; i++)
+            {
+                data2[i] = random.Next(0, 10000);
+            }
+            Array.Sort(data2);
+
+            Random random2 = new Random();
+            for (int i = 5000; i < data2.Length - 4000; i++)
+            {
+                data2[i] = random.Next(0, 10000);
+            }
+
+            for (int i = 0; i < data2.Length - 5000; i++)
+            {
+                data2[i] = random.Next(0, 10000);
+            }
+
+            size = data2.Length;
+
+            int[] result = SelectionSort_Class.Selectionsort(data2, size);
+
+            int[] expected = (int[])data2.Clone();
+            Array.Sort(expected);
+
+            CollectionAssert.AreEqual(expected, result);
+
         }
     }
+
+    /// <summary>
+    /// Our goal here is to correctly sort a randomly sorted string, to see the average method here.
+    /// </summary>
+
+    [TestClass]
+    public class AverageSelectionSortTests
+    {
+        [TestMethod]
+        public void TestSelectionSortMethod()
+        {
+            int size;
+
+            int[] data2 = new int[10000];
+            Random random = new Random();
+
+            for (int i = 0; i < data2.Length; i++)
+            {
+                data2[i] = random.Next(0, 10000);
+            }
+
+            size = data2.Length;
+
+            int[] result = SelectionSort_Class.Selectionsort(data2, size);
+
+            int[] expected = (int[])data2.Clone();
+            Array.Sort(expected);
+
+            CollectionAssert.AreEqual(expected, result);
+
+        }
+    }
+
+    /// <summary>
+    /// Our goal here is to correctly sort a reverse sorted string, to see the worst method here.
+    /// </summary>
+
+    [TestClass]
+    public class WorstSelectionSortTests
+    {
+        [TestMethod]
+        public void TestSelectionSortMethod()
+        {
+            int size;
+
+            int[] data2 = new int[10000];
+            Random random = new Random();
+
+            for (int i = 0; i < data2.Length; i++)
+            {
+                data2[i] = random.Next(0, 10000);
+            }
+
+            Array.Sort(data2);
+            Array.Reverse(data2);
+
+            size = data2.Length;
+
+            int[] result = SelectionSort_Class.Selectionsort(data2, size);
+
+            int[] expected = (int[])data2.Clone();
+            Array.Sort(expected);
+
+            CollectionAssert.AreEqual(expected, result);
+
+        }
+    }
+
 }
